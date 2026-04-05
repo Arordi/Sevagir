e4875218894f47af4978d7d24aa1f2056bb58e66
Arordi <arordi03@gmail.com>
2026-04-05 06:26:48 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+let words = [], activeStage = 1, activeIndex = 0, activeList = [], canProceed = false;
+// ... բոլոր ֆունկցիաները (initApp, renderStage1, և այլն) ...
+
+// Մի մոռացիր վերջում կանչել initApp();
+initApp();
